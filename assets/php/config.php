f47ae22f04ec4420646bb098cb31a6ab5d43a6df
Unknown Person <jonayedanimator@gmail.com>
2024-08-06 16:31:31 +0600

    Add files via upload

---
+++ assets/php/config.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+// mysqli database connection
+const DB_NAME = 'unknown_media';
+const DB_HOST = 'sql208.infinityfree.com';
+const DB_USER = 'if0_37025447';
+const DB_PASS = 'Jonayed2011';
